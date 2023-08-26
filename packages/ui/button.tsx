92504176980cf9a8260423a320505be3cbd1c50c
shadow <pieroramirez810@gmail.com>
2023-08-26 13:46:55 -0400

    chore: inital commit

--- packages/ui/button.tsx
+++ packages/ui/button.tsx
@@ -3,7 +3,7 @@
 export function Button(): JSX.Element {
   return (
     // eslint-disable-next-line no-alert
-    <button onClick={(): void => alert("booped")} type="button">
+    <button onClick={(): void => alert("boopedasdas")} type="button">
       Boop
     </button>
   );
