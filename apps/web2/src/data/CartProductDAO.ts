8d0a12891c6cb21ecc903d246ea977578f108158
shadow <pieroramirez810@gmail.com>
2023-09-16 11:17:53 -0400

    style: formatted cartDao

--- apps/web2/src/data/CartProductDAO.ts
+++ apps/web2/src/data/CartProductDAO.ts
@@ -1,37 +1,37 @@
 import { ICartProduct, type AppGamesDB } from "./index";
 
-export class CartProductDAO{
-    constructor(private db:AppGamesDB){}
+export class CartProductDAO {
+  constructor(private db: AppGamesDB) { }
 
-    async getTotalProductsQuantity(){
-        return await this.db.products.count()
+  async getTotalProductsQuantity() {
+    return await this.db.products.count()
+  }
+
+  async getTotalProductsPrice() {
+    return (await this.db.products.toArray())
+      .reduce((acc, product) => acc + product.price * product.quantity, 0)
+  }
+
+  async addProduct(product: ICartProduct) {
+    const productResult = await this.db.products.get({ productId: product.productId })
+
+    if (productResult?.id) {
+      return await this.changeQuantity(productResult.id, productResult.quantity + 1)
     }
 
-    async getTotalProductsPrice(){
-        return (await this.db.products.toArray())
-                .reduce((acc,product)=>acc+product.price*product.quantity,0)
-    }
+    return await this.db.products.add(product)
+  }
 
-    async addProduct(product:ICartProduct){       
-       const productResult = await this.db.products.get({productId:product.productId})
+  async deleteProduct(id: number) {
+    return await this.db.products.delete(id)
+  }
 
-       if(productResult?.id){
-        return await this.changeQuantity(productResult.id,productResult.quantity+1)
-       }
+  async changeQuantity(id: number, quantity: number) {
+    return await this.db.products.update(id, { quantity })
+  }
 
-       return await this.db.products.add(product)
-    }
-
-    async deleteProduct(id:number){
-        return await this.db.products.delete(id)
-    }
-
-    async changeQuantity(id:number,quantity:number){
-        return await this.db.products.update(id,{quantity})
-    }
-
-    async getProducts(){
-        return await this.db.products.toArray()
-    }
+  async getProducts() {
+    return await this.db.products.toArray()
+  }
 
 }
