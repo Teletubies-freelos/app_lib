2c252f201c74d47ffe641afcd4640a3848d5c2cc
shadow <pieroramirez810@gmail.com>
2023-09-24 00:01:16 -0400

    fix: tsc

--- packages/mock-service/src/factory/index.ts
+++ packages/mock-service/src/factory/index.ts
@@ -1,7 +1,6 @@
 import { factory } from '@mswjs/data';
 import { productsFactory } from './products';
 import { ordersFactory } from './orders';
-import { FactoryAPI } from '@mswjs/data/lib/glossary';
 
 export const db = factory(
   {
