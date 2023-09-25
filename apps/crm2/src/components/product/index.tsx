584e580dbfbb9f1fcb12798c7b2064ea40a4eacb
shadow <pieroramirez810@gmail.com>
2023-09-24 20:02:57 -0400

    feat: product modal

---
+++ apps/crm2/src/components/product/index.tsx
@@ -0,0 +1,70 @@
+import { Box, InputAdornment, TextField } from "@mui/material"
+import { useMutation } from "@tanstack/react-query";
+import { useContext } from "react";
+import { SubmitHandler, useForm } from "react-hook-form"
+import { dataContext } from "../../context/data";
+import { CreateGamesPayload } from "../../services/products";
+
+interface ProductData{
+  name: string;
+  price: number;
+  priceOffer?: number;
+  description?: string;
+}
+
+
+
+export const Product = ({name, price, description, priceOffer}: ProductData)=>{
+  const { register, handleSubmit } = useForm<ProductData>();
+
+  const { products } = useContext(dataContext)
+
+  const { mutate } = useMutation(async (payload: CreateGamesPayload)=> await products?.createOne(payload))
+
+  const onSubmit: SubmitHandler<ProductData> = ({description, name, price, priceOffer})=>{
+    mutate({
+      description,
+      name,
+      offer_price: priceOffer,
+      price
+    })
+  }
+
+  return(
+    <Box component={'form'} onSubmit={handleSubmit(onSubmit)} >
+      <TextField
+        defaultValue={name}
+        required 
+        variant='outlined'
+        label='Nombre' 
+        {...register('name')} 
+      />
+      <TextField
+        defaultValue={description}
+        variant='outlined'
+        label='Descripción'
+        {...register('description')}
+      />
+      <TextField 
+        required
+        defaultValue={price} 
+        variant='outlined' 
+        label='Precio' 
+        InputProps={{
+          startAdornment: <InputAdornment position="start" >S/.</InputAdornment>
+        }}
+        {...register('price')}
+      />
+      <TextField 
+        defaultValue={priceOffer}
+        variant='outlined' 
+        label='Precio oferta'
+        InputProps={{
+          startAdornment: <InputAdornment position="start" >S/.</InputAdornment>
+        }}
+        {...register('priceOffer')} 
+      />
+      
+    </Box>
+  )
+}
--- apps/crm2/src/services/products/index.ts
+++ apps/crm2/src/services/products/index.ts
@@ -11,18 +11,49 @@ export interface GamesResponse {
   image_url: string;
 }
 
+export interface CreateGamesPayload {
+  description?: string; 
+  imag_url?: string; 
+  name?: string; 
+  offer_price?: number;
+  quantity?: number;
+  price?: number
+}
+
 
 export class Products {
   static GET_LIST = gql`
-   query GET_PAGINATED_GAMES($limit: Int!, $offset: Int) {
-    games(limit: $limit, offset: $offset) {
-      description
-      id
-      img_url
-      price
-      name
+    query GET_PAGINATED_GAMES($limit: Int!, $offset: Int) {
+      games(limit: $limit, offset: $offset) {
+        description
+        id
+        img_url
+        price
+        name
+      }
     }
-  }
+  `
+
+  static CREATE_PRODUCT = gql`
+    mutation CREATE_PRODUCT(
+      $description: String,
+      $imag_url: String,
+      $name: String,
+      $price: Float,
+      $offer_price: Float,
+      $quantity: Int
+    ) {
+      insert_products_one(
+        object : { 
+          description: $description, 
+          imag_url: $imag_url, 
+          name: $name, 
+          offer_price: $offer_price,
+          quantity: $quantity
+        }){
+          id
+        }
+    }
   `
   constructor(private client: GraphQLClient){}
 
@@ -36,5 +67,14 @@ export class Products {
       .request<{games: GamesResponse[]}>(Products.GET_LIST, {limit, offset})
 
     return games
+  }
+
+  async createOne(payload : CreateGamesPayload){
+    
+    const { insert_products_one } = await this
+      .client
+      .request<{ insert_products_one: {id: string | number} }>(Products.CREATE_PRODUCT, {...payload})
+
+    return insert_products_one;
   }
 }
