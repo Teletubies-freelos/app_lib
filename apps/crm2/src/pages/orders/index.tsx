bf7908343b25fa352e650ed5f3fd3713916f5186
lgcarlinf <I202333437@cibertec.edu.pe>
2023-09-30 19:34:25 -0500

    chore: protect route orders

--- apps/crm2/src/pages/orders/index.tsx
+++ apps/crm2/src/pages/orders/index.tsx
@@ -1,14 +1,17 @@
 import { Stack, Typography } from '@mui/material';
 import ListOrders from './ListOrders';
+import { Authenticated } from '../../components/authenticated';
 
 const Orders = () => {
   return (
-    <Stack gap='1rem'>
-      <Typography variant='h1' padding='1rem'>
-        Registro de Ordenes
-      </Typography>
-      <ListOrders />
-    </Stack>
+    <Authenticated>
+      <Stack gap='1rem'>
+        <Typography variant='h1' padding='1rem'>
+          Registro de Ordenes
+        </Typography>
+        <ListOrders />
+      </Stack>
+    </Authenticated>
   );
 };
 
