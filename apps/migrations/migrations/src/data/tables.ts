ecc0a8c6d619c4eabf031c5c72e09e369a2ea887
Shadow <pieroramirez810@gmail.com>
2023-10-14 04:28:12 -0500

    migrations

---
+++ apps/migrations/migrations/src/data/tables.ts
@@ -0,0 +1,9 @@
+export enum Tables{
+  PRODUCT = 'product',
+  FEATURED = 'featured',
+  CATEGORY ='category',
+  ORDER = 'order',
+  ORDER_PRODUCT = 'order_product',
+  ORDER_STATUS = 'order_status',
+  PAYMENT_METHOD = 'payment_method'
+}
